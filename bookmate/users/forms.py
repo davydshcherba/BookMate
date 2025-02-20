e6612514a18682b5dee74d9c628dce09f949d07c
Eloy527 <davidserba302@gmail.com>
2025-02-20 19:51:34 +0200

    fax bags and add styles for register.html

--- bookmate/users/forms.py
+++ bookmate/users/forms.py
@@ -1,10 +1,15 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ["username", "email", "password1", "password2"]
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "input-field"}),
+            "email": forms.EmailInput(attrs={"class": "input-field"}),
+            "password1": forms.PasswordInput(attrs={"class": "input-field"}),
+            "password2": forms.PasswordInput(attrs={"class": "input-field"}),
+        }
+        help_texts = {field: "" for field in fields}  
--- bookmate/users/views.py
+++ bookmate/users/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib.auth import login
 from .forms import RegisterForm
 
 def register(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, "Ваш акаунт створено! Тепер ви можете увійти.")
-            return redirect('login')
+            user = form.save()
+            login(request, user)  
+            return redirect('home')  
     else:
         form = RegisterForm()
-    
     return render(request, 'users/register.html', {'form': form})
