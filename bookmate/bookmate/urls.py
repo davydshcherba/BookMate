212ead83321f614f23d0550787fb27df3be92126
Eloy527 <davidserba302@gmail.com>
2025-02-21 20:24:41 +0200

    added the ability to change username

--- bookmate/bookmate/urls.py
+++ bookmate/bookmate/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')), 
     path('users/', include('users.urls')), 
-    path('books/', include('books.urls')),
 ]
--- bookmate/users/views.py
+++ bookmate/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from .forms import RegisterForm
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
@@ -12,3 +14,18 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def changeusername(request):
+    if request.method == "POST":
+        new_username = request.POST.get("username")
+        if new_username:
+            request.user.username = new_username
+            request.user.save()
+            messages.success(request, "Username changed successfully!")
+            return redirect("home")  # або залишити на тій же сторінці `changeusername`
+        else:
+            messages.error(request, "Please enter a valid username.")
+
+    return render(request, 'users/changeUsername.html')
