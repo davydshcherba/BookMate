4622e30eb6c1dad819c61d2131a77dcc65d110bc
Eloy527 <davidserba302@gmail.com>
2025-02-21 19:35:35 +0200

    delete app books

--- bookmate/books/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('search/', views.search_books, name='search_books'),
-]
--- bookmate/books/views.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.conf import settings
-from django.shortcuts import render
-
-def search_books(request):
-    return render(request, 'books/search_results.html')
