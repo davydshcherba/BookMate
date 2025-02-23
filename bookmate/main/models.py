5bb71a447b5eed04a9286e476c9983faed8907e5
Eloy527 <davidserba302@gmail.com>
2025-02-23 10:34:06 +0200

    update styles and create new table book

--- bookmate/main/models.py
+++ bookmate/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField("Name Book", max_length=150)
+    pages = models.IntegerField("Pages in Book")
+    page_now = models.IntegerField("Page Now")
+    bookmarks = models.JSONField("Bookmarks", default=list)  
+    
+    def __str__(self):
+        return self.title
+
