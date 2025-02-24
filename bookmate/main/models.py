b3eb4fd964a07ce42c1d41dd3cbf51310afd49f6
Eloy527 <davidserba302@gmail.com>
2025-02-24 18:57:27 +0200

    Add tables book and bookmark and try to show it at home page

--- bookmate/main/models.py
+++ bookmate/main/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
     title = models.CharField("Name Book", max_length=150)
     pages = models.IntegerField("Pages in Book")
     page_now = models.IntegerField("Page Now")
-    bookmarks = models.JSONField("Bookmarks", default=list)  
-    
+
     def __str__(self):
         return self.title
 
+class Bookmark(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="bookmarks")
+    title = models.CharField("Name of Bookmark", max_length=150)
+    bookmark = models.TextField("Bookmark Content")
+
+    def __str__(self):
+        return f"{self.title} (Page {self.bookmark})"
--- bookmate/main/views.py
+++ bookmate/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Book
 
 def home(request):
-    return render(request, 'main/index.html')
+    books = Book.objects.filter(user=request.user) if request.user.is_authenticated else []
+    return render(request, "main/index.html", {"books": books})
+
 def profile(request):
     return render(request, 'main/profile.html')
