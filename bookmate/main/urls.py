7bd7d88967dec1e4ec79e1eb85f141164191cbd1
Eloy527 <davidserba302@gmail.com>
2025-02-25 21:38:57 +0200

    add ability add book

--- bookmate/main/urls.py
+++ bookmate/main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),  
     path('profile/', views.profile, name='profile'),
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
-    path('add-bookmark/', views.add_bookmark, name='add_bookmark'),  # Додано маршрут
+    path('add_bookmark/', views.add_bookmark, name='add_bookmark'),  
+    path('add_book/', views.add_book, name='add_book')
 ]
--- bookmate/main/views.py
+++ bookmate/main/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import F  # Імпортуємо F для порівняння полів у БД
 from .models import Book, Bookmark
 
 def home(request):
-    books = Book.objects.filter(user=request.user) if request.user.is_authenticated else []
+    if request.user.is_authenticated:
+        Book.objects.filter(user=request.user, page_now__gte=F("pages")).delete()       
+        books = Book.objects.filter(user=request.user)
+    else:
+        books = []
     return render(request, "main/index.html", {"books": books})
 
 def profile(request):
-    return render(request, 'main/profile.html')
+    return render(request, "main/profile.html")
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id, user=request.user)
-    return render(request, 'main/book_detail.html', {"book": book})
+    return render(request, "main/book_detail.html", {"book": book})
 
 def add_bookmark(request):
     if request.method == "POST":
@@ -21,7 +26,19 @@ def add_bookmark(request):
         book = get_object_or_404(Book, id=book_id, user=request.user)
         Bookmark.objects.create(book=book, title=title, bookmark=content)
 
-        return redirect("book_detail", book_id=book.id)  # Повернення до книги після додавання
+        return redirect("book_detail", book_id=book.id) 
 
     books = Book.objects.filter(user=request.user)
     return render(request, "main/add_bookmark.html", {"books": books})
+
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        pages = request.POST.get("pages")
+        page_now = request.POST.get("page_now")
+
+        if request.user.is_authenticated:
+            Book.objects.create(user=request.user, title=title, pages=pages, page_now=page_now)
+            return redirect("home")  
+
+    return render(request, "main/add_book.html")
